ac63be8465b5ef526c0dccc0ce30becc4f25d3f6
marchrys <mar.chrysikopoulos@gmail.com>
2020-11-01 17:20:34 +0100

    init project and added react navigation

---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+
+const Home = ({ navigation }) => {
+
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+};
+
+export default Home;
